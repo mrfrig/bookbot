e68a624eec8f8dddfa9571bf0364cac481cc7e92
Franklin Isalguez <franklin@neosmail.com>
2025-05-23 09:26:28 -0400

    2-3 add get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    file_contents = ""
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    print(text)
+
+
+main()
